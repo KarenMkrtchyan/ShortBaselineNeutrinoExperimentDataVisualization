7ced364fc2d0957d93db89a682fd6393e80acc7c
Karen Mkrtchyan <karenmkrtchyanarmenia@gmail.com>
2024-07-09 11:52:08 -0500

    width WORKS for both amp and width

--- width.C
+++ width.C
@@ -1,5 +1,6 @@
 void width() {
     // ROOT file name
+    //--------------CHANGE FILE NAME--------------//
     #define filename "Supp_Stage0_55.root"
 
     TFile *file = new TFile(filename, "READ");
@@ -24,7 +25,9 @@ void width() {
         return;
     }
 
-    // Open output file in UPDATE mode
+    //------------CHANGE OUTPUT FILE NAME------------------//
+    //RECREATE will overwrite the file, use this the first time executing
+    //UPDATE will add on to the file, use this ever subsequent run 
     TFile *outputFile = new TFile("out.root", "RECREATE");
     if (!outputFile->IsOpen()) {
         std::cerr << "Error: Cannot create or open output file!" << std::endl;
@@ -33,49 +36,52 @@ void width() {
     }
 
     // Automate the plotting of each plane and TPC
-    const char *format = "TPC==%d&&Plane==%d";
+    const char *format = "TPC==%d && Plane==%d";
     const char *formatTitle = "TPC %d, Plane %d";
     char formattedString[100];
     char formattedString2[100];
-    string drawParam[]= {"Width", "Amplitude"};
-
 
     // Disable statistics box globally
     gStyle->SetOptStat(0);
-    TCanvas *c1 = new TCanvas("c1", "Width Histograms", 800, 600);
+    string drawParam[]= {"Width", "Amplitude"};
+    TCanvas *c1 = new TCanvas("c1", "Width Histograms", 800, 600); //needed so h can be properly selected later on
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            snprintf(formattedString, sizeof(formattedString), format, i, j);
+for(int k =0; k<2; k++){//Draw param (do not ask why it didn't work being the 3rd loop in the nest)
+    for (int i = 0; i < 3; i++) { //TPC
+        for (int j = 0; j < 3; j++) { //Plane
+            snprintf(formattedString, sizeof(formattedString), format, i, j); // for logical selection
             snprintf(formattedString2, sizeof(formattedString2), formatTitle, i, j); // For title
 
-            for(int k =0; k<2; k++){
             std::string param = drawParam[k];
             std::string drawCommand = param + ">>htemp";
+            //For Debugging purposes 
             std::cout << "Drawing: " << drawCommand << " with selection: " << formattedString << std::endl;
-            
+         
+            // Draw the histogram
             tree->Draw(drawCommand.c_str(), formattedString);
             TH1 *h = (TH1*)gDirectory->Get("htemp");
 
-            // Handle the case where h is NULL
+            // Handle the case where h is NULL (some files are missing TPC 3)
             if (h) {
                 char name[100];
-                snprintf(name, sizeof(name), "Width_TPC%d_Plane%d", i, j);
+                snprintf(name, sizeof(name), "%s_TPC%d_Plane%d", param.c_str(), i, j);
                 h->SetName(name);
                 h->SetTitle(formattedString2);
-                h->GetXaxis()->SetTitle("Hit Width [ticks]");
+                if(k==0){
+                    h->GetXaxis()->SetTitle("Hit Width [ticks]");
+                }else{
+                     h->GetXaxis()->SetTitle("Amplitude [ADC]");
+                }   
                 h->GetYaxis()->SetTitle("Hits");
                 h->GetYaxis()->SetTitleOffset(1.3);
                 h->Write();
             } else {
                 std::cerr << "Warning: Histogram for TPC " << i << " Plane " << j << " not created (empty selection)." << std::endl;
             }
-
         }
     }
-
+}
     // Close the files
     outputFile->Close();
     file->Close();
-    }
 }
