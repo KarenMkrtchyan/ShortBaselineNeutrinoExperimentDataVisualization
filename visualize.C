2bb686455dcb1bebb62823bdf4681bfae22d9df4
Karen Mkrtchyan <karenmkrtchyanarmenia@gmail.com>
2024-07-12 13:23:21 -0500

    false alert, issue was with data not being modifed in the frist place

--- visualize.C
+++ visualize.C
@@ -9,7 +9,8 @@ void visualize() {
     //------------CHANGE OUTPUT FILE NAME------------------//
     //RECREATE will overwrite the file, use this the first time executing
     //UPDATE will add on to the file, use this ever subsequent run 
-    TFile *outputFile = new TFile("out.root", "RECREATE");
+    #define outFile "out.root"
+    TFile *outputFile = new TFile(outFile, "RECREATE");
     if (!outputFile->IsOpen()) {
         std::cerr << "Error: Cannot create or open output file!" << std::endl;
         return;
@@ -23,7 +24,7 @@ void visualize() {
     char formattedString2[100];
 
     //needed for historgrams to merge together propely
-    int nbinsx = 100;
+    int nbinsx = 150;
     int xlow = 0;
     int xup = 15;
 
@@ -79,69 +80,71 @@ void visualize() {
             return;
         }
        }
-        TFile *outputFileReal  = new TFile("out.root", "UPDATE"); //even Lisan Al Gaib doesn't know why this line is neccessary 
-        for(int k =0; k<3; k++){ // Draw param 
-            for (int i = 0; i < 2; i++) { // TPC
-                for (int j = 0; j < 3; j++) { // Plane         
-                    snprintf(formattedString, sizeof(formattedString), format, i, j); // for logical selection
-                    snprintf(formattedString2, sizeof(formattedString2), formatTitle, i, j); // For title
-                    std::string param = drawParam[k];
-                    std::string drawCommand = param + ">>htemp(" + std::to_string(nbinsx) + "," + std::to_string(xlow) + "," + std::to_string(xup) + ")";
-                    std::cout << "Drawing: " << drawCommand << " with selection: " << formattedString << std::endl; 
-                    if(k==2){
-                        drawCommand = "Amplitude:Width>>htemp(" + std::to_string(nbinsx) + "," + std::to_string(xlow) + "," + std::to_string(xup) + "," + std::to_string(nbinsx) + "," + std::to_string(xlow) + "," + std::to_string(xup) + ")";
-                        tree->Draw(drawCommand.c_str(), formattedString, "colz");
-                    }else{
-                        tree->Draw(drawCommand.c_str(), formattedString);
-                    }              
-                    TH1 *h = (TH1*)gDirectory->Get("htemp");
-
+       //change directory into output file 
+    //    gSystem->cd(outFile);
+       TFile *outputFileReal  = new TFile("out.root", "UPDATE"); //even Lisan Al Gaib doesn't know why this line is neccessary 
+       for(int k =0; k<3; k++){ // Draw param 
+           for (int i = 0; i < 2; i++) { // TPC
+               for (int j = 0; j < 3; j++) { // Plane         
+                   snprintf(formattedString, sizeof(formattedString), format, i, j); // for logical selection
+                   snprintf(formattedString2, sizeof(formattedString2), formatTitle, i, j); // For title
+                   std::string param = drawParam[k];
+                   std::string drawCommand = param + ">>htemp(" + std::to_string(nbinsx) + "," + std::to_string(xlow) + "," + std::to_string(xup) + ")";
+                   std::cout << "Drawing: " << drawCommand << " with selection: " << formattedString << std::endl; 
+                   if(k==2){
+                       drawCommand = "Amplitude:Width>>htemp(" + std::to_string(nbinsx) + "," + std::to_string(xlow) + "," + std::to_string(xup) + "," + std::to_string(nbinsx) + "," + std::to_string(xlow) + "," + std::to_string(xup) + ")";
+                       tree->Draw(drawCommand.c_str(), formattedString, "colz");
+                   }else{
+                       tree->Draw(drawCommand.c_str(), formattedString);
+                   }              
+                   TH1 *h = (TH1*)gDirectory->Get("htemp");
                     // Handle the case where h is NULL 
-                    if (h) {
-                        char name[100];
-                        if(l==0){ // tracks
-                            snprintf(name, sizeof(name), "%s_TPC%d_Plane%d", param.c_str(), i, j);
-                        }
-                        else if(l==1){ // modified tracks
-                            snprintf(name, sizeof(name), "MOD_%s_TPC%d_Plane%d", param.c_str(), i, j);
-                        }
-                        else{ // data tracks
-                            snprintf(name, sizeof(name), "DATA_%s_TPC%d_Plane%d", param.c_str(), i, j);
-                        }
-                        h->SetName(name);
-                        h->SetTitle(formattedString2);
-                        if(k==0){
-                            h->GetXaxis()->SetTitle("Hit Width [ticks]");
-                            h->GetYaxis()->SetTitle("Hits");
-                        }else if(k==1){
-                            h->GetXaxis()->SetTitle("Amplitude [ADC]");
-                            h->GetYaxis()->SetTitle("Hits");
-                        }else{
-                             if(l==0){ // tracks
-                            snprintf(name, sizeof(name), "Amplitude_Width_TPC%d_Plane%d",  i, j);
-                        }
-                        else if(l==1){ // modified tracks
-                            snprintf(name, sizeof(name), "MOD_Amplitude_Width_TPC%d_Plane%d",  i, j);
-                        }
-                        else{ // data tracks
-                            snprintf(name, sizeof(name), "DATA_Amplitude_Width_TPC%d_Plane%d",  i, j);
-                        }
-                            h->SetName(name);
-                            h->SetTitle(formattedString2);
-                            h->GetXaxis()->SetTitle("Hit Width [ticks]");
-                            h->GetYaxis()->SetTitle("Amplitude [ADC]"); 
-                        }                      
-                        h->GetYaxis()->SetTitleOffset(1.3);
-                        // std::string newName = std::string(name)+".pdf";
-                        // c1->SaveAs(newName.c_str()); //prints our plots as pdfs
-                        h->Write();
-                    } else {
-                        std::cerr << "Warning: Histogram for TPC " << i << " Plane " << j << " not created (empty selection)." << std::endl;
-                    }
-                }
-            }
-        }
-        file->Close();
-        outputFileReal->Close();
-    }
+                   if (h) {
+                       char name[100];
+                       if(l==0){ // tracks
+                           snprintf(name, sizeof(name), "%s_TPC%d_Plane%d", param.c_str(), i, j);
+                       }
+                       else if(l==1){ // modified tracks
+                           snprintf(name, sizeof(name), "MOD_%s_TPC%d_Plane%d", param.c_str(), i, j);
+                       }
+                       else{ // data tracks
+                           snprintf(name, sizeof(name), "DATA_%s_TPC%d_Plane%d", param.c_str(), i, j);
+                       }
+                       h->SetName(name);
+                       h->SetTitle(formattedString2);
+                       if(k==0){
+                           h->GetXaxis()->SetTitle("Hit Width [ticks]");
+                           h->GetYaxis()->SetTitle("Hits");
+                       }else if(k==1){
+                           h->GetXaxis()->SetTitle("Amplitude [ADC]");
+                           h->GetYaxis()->SetTitle("Hits");
+                       }else{
+                            if(l==0){ // tracks
+                           snprintf(name, sizeof(name), "Amplitude_Width_TPC%d_Plane%d",  i, j);
+                       }
+                       else if(l==1){ // modified tracks
+                           snprintf(name, sizeof(name), "MOD_Amplitude_Width_TPC%d_Plane%d",  i, j);
+                       }
+                       else{ // data tracks
+                           snprintf(name, sizeof(name), "DATA_Amplitude_Width_TPC%d_Plane%d",  i, j);
+                       }
+                           h->SetName(name);
+                           h->SetTitle(formattedString2);
+                           h->GetXaxis()->SetTitle("Hit Width [ticks]");
+                           h->GetYaxis()->SetTitle("Amplitude [ADC]"); 
+                       }                      
+                       h->GetYaxis()->SetTitleOffset(1.3);
+                       // std::string newName = std::string(name)+".pdf";
+                       // c1->SaveAs(newName.c_str()); //prints our plots as pdfs
+                       h->Write();
+                   } else {
+                       std::cerr << "Warning: Histogram for TPC " << i << " Plane " << j << " not created (empty selection)." << std::endl;
+                   }
+               }
+           }
+       }
+       file->Close();
+       outputFileReal->Close();
+   }
+   #undef outFile
 }
